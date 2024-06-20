9d1cbc94ac5e617cd99a3c79d6cbc686adec355f
Zeena Yalda <zyalda@hotmail.com>
2024-06-20 11:07:40 +0200

    Add new repository file to get the entity data reader file

---
+++ BusinessLayer/Services/RepositoryFactory.cs
@@ -0,0 +1,35 @@
+﻿using CommonLayer.Interfaces;
+using CommonLayer.Models;
+using DataAccess.Services;
+using System;
+
+namespace BusinessLayer.Services
+{
+    public static class RepositoryFactory
+    {
+        public static IRepository<T> GetRepository<T>(string repositoryType) where T : class
+        {
+            IRepository<T> repository = null;
+
+            switch (repositoryType)
+            {
+                case "Employee":
+                    IRepository<Employee> repositoryEmployee = new EmployeeDataReader();
+                    repository = (IRepository<T>)repositoryEmployee;
+                    break;
+                case "Customer":
+                    IRepository<Customer> repositoryCustomer = new CustomerDataReader();
+                    repository = (IRepository<T>)repositoryCustomer;
+                    break;
+                case "Person":
+                    IRepository<Person> repositoryPeople = new PeopleDataReader();
+                    repository = (IRepository<T>)repositoryPeople;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid repository type");
+            }
+
+            return repository;
+        }
+    }
+}
